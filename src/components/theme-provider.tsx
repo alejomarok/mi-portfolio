84f45d07464994fbbe75ed8dc6c3f20d9138842d
Alejo Marco <alejom1999@gmail.com>
2025-03-26 01:49:01 -0300

    arreglando errores de consola.

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -1,8 +1,19 @@
 "use client"
-import { ThemeProvider as NextThemesProvider } from "next-themes"
+
+import { ThemeProvider as NextThemesProvider, useTheme } from "next-themes"
 import type { ThemeProviderProps } from "next-themes"
+import { useEffect, useState } from "react"
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+  const [mounted, setMounted] = useState(false)
+
+  useEffect(() => {
+    setMounted(true)
+  }, [])
+
+  if (!mounted) {
+    return <div style={{ visibility: "hidden" }}>{children}</div>
+  }
+
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>
 }
-
